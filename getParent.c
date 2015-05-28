3257d3f25de73fa2f30ff426a5a2cac94b96fdee
Alice <alice.writes.wrongs@gmail.com>
2015-05-28 16:16:07 -0400

    Trying out some stuff

--- getParent.c
+++ getParent.c
@@ -4,14 +4,15 @@
 void getParent(char hash[], char parents[], char dir[])
 {
     char contents[MAXLINE];
+    char parentLine[MAXLINE];
     char scratch[MAXLINE];
 
     commitContents(hash, contents);
 
-    
+    //sscanf(commitContents, "%s", scratch);
 
-
-    printf("%s\n", contents);
+    printf("%s\n", commitContents);
+    printf("%s\n", scratch);
 
     
 
