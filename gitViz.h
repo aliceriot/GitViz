34d000fe890dd6a8423bc5cbfab8ed238d1ed876
Alice <alice.writes.wrongs@gmail.com>
2015-06-10 13:16:01 -0400

    New functions

--- gitViz.h
+++ gitViz.h
@@ -3,6 +3,12 @@
 
 #define MAXLINE 1000
 
+typedef struct node node;
+struct node {
+    char commitHash[42];
+    node *children;
+};
+
 void headRef(char hash[], char dir[]);
 void hashLocation(char hash[], char location[], char dir[]);
 void commitContents(char hash[], char contents[]);
@@ -11,6 +17,8 @@ void getParent(char hash[], char parents[], char dir[]);
 void printGraph(char cwd[], int numlines);
 void getFirstSix(char head[], char smallHead[]);
 int getHashes(char buffer[], char *hashes[]);
+int exists(char hash[]);
+void lineformat(int numline);
 
 
 // ~*~ to implement ~*~ \\
